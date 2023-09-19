1df938fbf4859987bb48cb17d9f277697f0ebee3
TFlexSoom <tchilbert@live.com>
2023-09-19 13:28:02 -0700

    Add Duo Scaffolding

--- src/screens/Duo.tsx
+++ src/screens/Duo.tsx
@@ -3,19 +3,26 @@
  */
 
 import React from 'react';
+import { useEffect } from 'react';
 import {
     SafeAreaView,
-    ScrollView,
-    StatusBar,
+    Image,
 } from 'react-native';
 
+function duoAuthenticate() {
+    console.log("DUO AUTH HERE!");
+}
+
 export default function Duo(): JSX.Element {
+
+    useEffect(duoAuthenticate);
     return (
         <SafeAreaView>
-            <StatusBar />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic">
-            </ScrollView>
+            <Image source={{
+                uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2" +
+                    "FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgY" +
+                    "GBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==",
+            }} />
         </SafeAreaView>
     )
 }
